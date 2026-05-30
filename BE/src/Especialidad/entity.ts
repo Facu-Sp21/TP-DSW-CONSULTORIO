5ebb3f0a7b7b2666cc66c193d1729aa4af24480c
Facundo Speranza <facundosperanza21@hotmail.com>
2026-05-30 02:51:44 -0300

    Realize el CRUD completo sin persistencia del modulo de ESPECIALIDAD

---
+++ BE/src/Especialidad/entity.ts
@@ -0,0 +1,4 @@
+export interface Especialidad {
+  cod_especialidad?: number;
+  nombre?: string;
+}
---
+++ BE/src/Especialidad/repository.ts
@@ -0,0 +1,7 @@
+import { Especialidad } from './entity.js';
+
+export const dbEspecialidades: Especialidad[] = [
+    { cod_especialidad: 1, nombre: 'Cardiología' },
+    { cod_especialidad: 2, nombre: 'Dermatología' },
+    { cod_especialidad: 3, nombre: 'Neurología' },
+]
